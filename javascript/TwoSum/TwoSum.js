4f02d0ba12ba97e52b76f678855d339416aec1db
crux wild <18210841568@163.com>
2017-07-22 23:04:01 +0800

    add

---
+++ javascript/TwoSum/TwoSum.js
@@ -0,0 +1,26 @@
+/**
+ * @see https://leetcode.com/problems/two-sum/#/description
+ */
+
+var TwoSum;
+
+/**
+ * @constructor
+ * @param {Array<Number>} 任意数字类型的数组
+ * @param {Number} 数字类型的数组任意两个数字求和的结果
+ */
+TwoSum = function TwoSumF(numbers, target) {
+  this.numbers = numbers;
+  this.target = target;
+}
+
+/**
+ * @method
+ * @return {Array<Number>} 与求和结果匹配的数组两个元素的下标
+ */
+TwoSum.prototype.getIndices = function getIndicesF() {
+  var integerArray = this.integerArray;
+  var target = this.target;
+
+  return indices;
+}
