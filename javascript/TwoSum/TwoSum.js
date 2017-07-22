fb3eaa63755af0a4eea013fca80915c72b3506f1
crux wild <18210841568@163.com>
2017-07-22 23:07:55 +0800

    add

--- javascript/TwoSum/TwoSum.js
+++ javascript/TwoSum/TwoSum.js
@@ -6,8 +6,8 @@ var TwoSum;
 
 /**
  * @constructor
- * @param {Array<Number>} 任意数字类型的数组
- * @param {Number} 数字类型的数组任意两个数字求和的结果
+ * @param {Array<Number>} numbers - 任意数字类型的数组
+ * @param {Number} targer - 数字类型的数组任意两个数字求和的结果
  */
 TwoSum = function TwoSumF(numbers, target) {
   this.numbers = numbers;
@@ -19,8 +19,10 @@ TwoSum = function TwoSumF(numbers, target) {
  * @return {Array<Number>} 与求和结果匹配的数组两个元素的下标
  */
 TwoSum.prototype.getIndices = function getIndicesF() {
-  var integerArray = this.integerArray;
+  var numbers = this.numbers;
   var target = this.target;
+
+  var indices = [];
 
   return indices;
 }
