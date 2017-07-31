33ba79c75fbca31d179a4ae7a284641f0175bd85
crux wild <18210841568@163.com>
2017-07-31 22:12:25 +0800

    add

--- javascript/two-sum/spec/chained-hash-table.spec.js
+++ javascript/two-sum/spec/chained-hash-table.spec.js
@@ -18,19 +18,13 @@ test('Chained hash table: [Insert]', (it) => {
   const key = 1;
   let list;
 
+  // 插入一个不存在的键，自动链表保存对应值
   list = hashTable
     .insert({ key, value })
     .search({ key });
 
   it.deepEqual(list, [value],
-    '[HashTable] After insert value, list should be equals [value]');
-
-  list = hashTable
-    .insert({ key, value })
-    .search({ key });
-
-  it.deepEqual(list, [value, value],
-    '[HashTable] After insert value, list should be [value, value]');
+    '[HashTable] After first insert value, list should be equals [value]');
 });
 
 test('Chained hash table: [Delete]', (it) => {
@@ -48,6 +42,7 @@ test('Chained hash table: [Delete]', (it) => {
   it.deepEqual(list, [value1, value2],
     '[HashTable] After insert [value1, value2], list should be [value1, value2]');
 
+  // 删除可以对应键和值，删除对应键的链表中一项
   list = hashTable
     .delete({ key, value: value1 })
     .search({ key });
@@ -55,10 +50,23 @@ test('Chained hash table: [Delete]', (it) => {
   it.deepEqual(list, [value2],
     '[HashTable] After delete value1, list should be [value2]');
 
+  // 删除可以对应键和值，删除对应键的链表中一项
   list = hashTable
     .delete({ key, value: value2 })
     .search({ key });
 
   it.deepEqual(list, [],
     '[HashTable] After delete value2, list should be []');
+});
+
+test('Chained hash table: [Search]', (it) => {
+  const { hashTable }= it.context;
+  const key = 1;
+  let list;
+
+  list = hashTable.search({ key });
+
+  // 查找不存在的键值应该返回空数组
+  it.deepEqual(list, [],
+    '[HashTable] search unexist key, list should be []');
 });
