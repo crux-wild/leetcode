b097c5a11d617fa98ce4dbe4adfd5e1a8ad84274
crux wild <18210841568@163.com>
2017-07-31 23:43:43 +0800

    add

--- scala/median-of-two-sorted-arrays/src/main/scala/com/leetcode/scala/Section.scala
+++ scala/median-of-two-sorted-arrays/src/main/scala/com/leetcode/scala/Section.scala
@@ -5,20 +5,20 @@ class Section {
   private var _end = 0
 
   def start = _start
-  def start_ (newStart: Int): Unit = {
-    if (_start <= _end) {
-      _start = newStart
-    } else {
-      // @TODO 操作处理
-    }
+  def start_= (newStart: Int): Unit = {
+    //if (_start <= _end) {
+      //_start = newStart
+    //} else {
+      //// @TODO 操作处理
+    //}
   }
 
   def end = _end
-  def end_ = (newEnd: Int): Unit = {
-    if (_start <= _end) {
-      _end = newEnd;
-    } else {
-      // @TODO 操作处理
-    }
+  def end_= (newEnd: Int): Unit = {
+    //if (_start <= _end) {
+      //_end = newEnd
+    //} else {
+      //// @TODO 操作处理
+    //}
   }
 }
