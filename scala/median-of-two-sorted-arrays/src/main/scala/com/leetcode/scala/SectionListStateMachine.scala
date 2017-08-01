2ad3d4f1c9b0c82d51dddf04e6a366762d251260
crux wild <18210841568@163.com>
2017-08-01 19:45:12 +0800

    add

--- scala/median-of-two-sorted-arrays/src/main/scala/com/leetcode/scala/SectionListStateMachine.scala
+++ scala/median-of-two-sorted-arrays/src/main/scala/com/leetcode/scala/SectionListStateMachine.scala
@@ -7,5 +7,5 @@ import com.leetcode.scala.ElementPointer
 
 class SectionListStateMachine {
   private val sectionList = new MutableList[Section]()
-  private val pointer = new ElementPointer();
+  private val elementPointer = new ElementPointer();
 }
