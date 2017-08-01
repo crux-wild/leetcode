7adb24ffafb50b7a409f94e750ff03472c128fef
crux wild <18210841568@163.com>
2017-08-01 19:38:24 +0800

    add

---
+++ scala/median-of-two-sorted-arrays/src/main/scala/com/leetcode/scala/SectionListStateMachine.scala
@@ -0,0 +1,11 @@
+package com.leetcode.scala
+
+import scala.collection.mutable.MutableList
+
+import com.leetcode.scala.Section
+import com.leetcode.scala.SectionPointer
+
+class SectionListStateMachine {
+  private val sectionList = new MutableList[Section]()
+  private val pointer = new SectionPointer();
+}
---
+++ scala/median-of-two-sorted-arrays/src/main/scala/com/leetcode/scala/SectionPointer.scala
@@ -0,0 +1,5 @@
+package com.leetcode.scala
+
+class SectionPointer (
+  var index: Int =  0, var block: Int = 0, var offset: Int = 0) {
+}
