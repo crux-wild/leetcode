c81583fad8083559a46a0fe89f94d824e055aac1
crux wild <18210841568@163.com>
2017-08-01 19:41:45 +0800

    add

---
+++ scala/median-of-two-sorted-arrays/src/main/scala/com/leetcode/scala/ElementPointer.scala
@@ -0,0 +1,5 @@
+package com.leetcode.scala
+
+class ElementPointer (
+  var array: Int =  0, var range: Int = 0, var offset: Int = 0) {
+}
--- scala/median-of-two-sorted-arrays/src/main/scala/com/leetcode/scala/SectionPointer.scala
+++
@@ -1,5 +0,0 @@
-package com.leetcode.scala
-
-class SectionPointer (
-  var index: Int =  0, var block: Int = 0, var offset: Int = 0) {
-}
