0f510490aa52e746cd73fda4908e0c066a6680ec
crux wild <18210841568@163.com>
2017-08-01 18:33:58 +0800

    add

---
+++ scala/median-of-two-sorted-arrays/src/main/scala/com/leetcode/scala/ArrayStateMachine.scala
@@ -0,0 +1,12 @@
+package com.leetcode.scala;
+
+import scala.collection.mutable.ArrayBuffer
+
+import com.leetcode.scala.Median
+import com.leetcode.scala.Section
+
+class MedianOfTwoSortedArray(array: Array[Int]) {
+  private val _arrayBuffer = new ArrayBuffer()
+  private val _range = new Section()
+  private val _median = new Median()
+}
