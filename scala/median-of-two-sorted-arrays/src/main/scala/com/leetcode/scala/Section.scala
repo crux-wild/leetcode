b3c464be54dd20233d6d8469ea450a13ea326f99
crux wild <18210841568@163.com>
2017-08-02 22:55:31 +0800

    add

--- scala/median-of-two-sorted-arrays/src/main/scala/com/leetcode/scala/Section.scala
+++ scala/median-of-two-sorted-arrays/src/main/scala/com/leetcode/scala/Section.scala
@@ -1,9 +1,12 @@
 package com.leetcode.scala
 
-class Section(val start: Int = 0, val end: Int = 0) {
-  private var _start = start
-  private var _end = end
+import com.leetcode.scala.SectionCompare
 
+class Section(val initStart: Int = 0, val initEnd: Int = 0) {
+  private var _start = initStart
+  private var _end = initEnd
+
+  def start = _start
   def start_= (newStart: Int): Unit = {
     if (newStart <= _end) {
       _start = newStart
@@ -12,11 +15,79 @@ class Section(val start: Int = 0, val end: Int = 0) {
     }
   }
 
+  def end = _end
   def end_= (newEnd: Int): Unit = {
     if (_start <= newEnd) {
       _end = newEnd
     } else {
       throw new Exception("End should be geater than or equal start")
     }
+  }
+}
+
+object Section {
+  def compareSection(
+    section1: Section, section2: Section): SectionCompare.Value = {
+    /**
+     * |<-- section1 --->||<-- section2 --->|
+     */
+    if (section1.end <= section2.start) {
+      return SectionCompare.LESSER
+    }
+
+    /**
+     * |<-- section2 --->||<-- section1 --->|
+     */
+    if (section1.start >= section2.end) {
+      return SectionCompare.GREATER
+    }
+
+    /**
+     *   |<-- section1 -->|
+     * |<--   section2   -->|
+     */
+    if (section1.start > section2.start && section1.end < section2.end) {
+      return SectionCompare.BE_INCLUDED
+    }
+
+    /**
+     * |<--   section1   -->|
+     *   |<-- section2 -->|
+     *
+     */
+    if (section1.start < section2.start && section1.end > section2.end) {
+      return SectionCompare.INCLUDED
+    }
+
+    /**
+     * |<-- section1 -->|
+     * |<-- section2 -->|
+     */
+    if (section1.start == section2.start&& section1.end == section2.end) {
+      return SectionCompare.EQUAL
+    }
+
+
+    /**
+     * |<-- section1 -->|
+     *            |<-- section2 -->|
+     */
+    if (section1.start <= section2.start && section1.end < section2.end
+      && section1.end <= section2.start) {
+
+      return SectionCompare.END_INCLUDED
+    }
+
+   /**
+     *         |<-- section1 -->|
+     * |<-- section2 -->|
+     */
+    if (section1.start >= section2.start && section2.end <= section1.end
+      && section2.end < section1.start) {
+
+      return SectionCompare.START_INCLUDED
+    }
+
+    return SectionCompare.NO_RELATIVE
   }
 }
