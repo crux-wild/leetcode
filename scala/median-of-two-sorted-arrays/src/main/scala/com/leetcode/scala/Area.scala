f5c07b8af327001fa3bb880a141ecca03affa556
crux wild <18210841568@163.com>
2017-08-03 11:57:17 +0800

    add

--- scala/median-of-two-sorted-arrays/src/main/scala/com/leetcode/scala/Area.scala
+++ scala/median-of-two-sorted-arrays/src/main/scala/com/leetcode/scala/Area.scala
@@ -2,8 +2,10 @@ package com.leetcode.scala
 
 import com.leetcode.scala.Section
 
-class Area(val index: Int = 0, val section: Section, val count: Int) {
+class Area(val index: Int = 0, val start: Int, val end: Int, val count: Int) {
   private var _count = count
   private var _index = index
-  private var _section = section
+  private var _section = new Section(start, end)
+
+  def section = _section
 }
--- scala/median-of-two-sorted-arrays/src/main/scala/com/leetcode/scala/MedianOfTwoSortedArrays.scala
+++ scala/median-of-two-sorted-arrays/src/main/scala/com/leetcode/scala/MedianOfTwoSortedArrays.scala
@@ -3,23 +3,26 @@ package com.leetcode.scala
 import scala.math
 
 import com.leetcode.scala.MixtrueListStateMachine
-import com.leetcode.scala.Section
 import com.leetcode.scala.Area
+import com.leetcode.scala.Mixtrue
 
-class MedianOfTwoSortedArrays(val array1: Array[Int], val array2: Array[Int]) {
-  private val _areaListStateMachine = new MixtrueListStateMachine()
-  private val _array1 = array1
-  private val _array2 = array2
+class MedianOfTwoSortedArrays(val arr1: Array[Int], val arr2: Array[Int]) {
+  private val _mixtrueListStateMachine = new MixtrueListStateMachine()
+  private val _arr1 = arr1
+  private val _arr2 = arr2
 
-  private def initToMergeAreaListStateMachine(): Unit = {
-    val length1 = _array1.length
-    val length2 = _array2.length
-    val maxLength = math.max(length1, length2)
+  private def initMixtrueListStateMachine(): Unit = {
+    val len1 = _arr1.length
+    val len2 = _arr2.length
+    val maxLen = math.max(len1, len2)
 
-    if (maxLength == length1) {
-
+    // 选取区间范围较大的数组作为参照
+    if (maxLen == len1) {
+       _mixtrueListStateMachine +=
+          new Area(index = 1, start = _arr1.head, end = _arr1.last, count = len1)
     } else {
-
+       _mixtrueListStateMachine +=
+          new Area(index = 2, start = _arr2.head, end = _arr2.last, count = len2)
     }
   }
 
