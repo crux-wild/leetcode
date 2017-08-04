b6b369d254d50640880d270c577bab6f8dfac155
crux wild <18210841568@163.com>
2017-08-05 00:05:39 +0800

    remove

--- c-language/find-substring/find-substring.c
+++
@@ -1,7 +0,0 @@
-/**
- * Return an array of size *returnSize.
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int * findSubstring(char * const s, char ** const words, int const words_size,
-  int * return_size) {
-}
--- c-language/find-substring/knuth-morris-pratt.h
+++
@@ -1,8 +0,0 @@
-typedef struct KmpMatcher {
-  int * prefixFunction;
-  int patternLength;
-  char * text;
-  char * pattern;
-} KmpMatcher;
-
-KmpMatcher * createKmpMatcher(char * text, char * pattern);
