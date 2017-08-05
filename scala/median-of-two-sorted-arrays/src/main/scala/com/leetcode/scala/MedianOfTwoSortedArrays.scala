01cbaadd0dd5ca5062dbbd038e25e6da081ced92
crux wild <18210841568@163.com>
2017-08-05 19:41:14 +0800

    add

--- scala/median-of-two-sorted-arrays/src/main/scala/com/leetcode/scala/MedianOfTwoSortedArrays.scala
+++ scala/median-of-two-sorted-arrays/src/main/scala/com/leetcode/scala/MedianOfTwoSortedArrays.scala
@@ -3,4 +3,7 @@ package com.leetcode.scala
 class MedianOfTwoSortedArrays[T](val arr1: Array[T], val arr2: Array[T]) {
   private val _arr1 = arr1
   private val _arr2 = arr2
+  private var _median = 0
+
+  def median = _median
 }
