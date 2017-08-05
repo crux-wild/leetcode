8609e1999fa0cde1e765c68244334d8d968058dc
crux wild <18210841568@163.com>
2017-08-05 13:00:44 +0800

    add

--- scala/median-of-two-sorted-arrays/src/main/scala/com/leetcode/scala/Section.scala
+++ scala/median-of-two-sorted-arrays/src/main/scala/com/leetcode/scala/Section.scala
@@ -4,26 +4,32 @@ import _root_.scala.math
 
 class Median(val start: Int, val end: Int) {
   private val _count = getCount()
-  private val _isEvenNumber = isEvenNumber(_count)
+  private val _isEven = isEvenNumber(_count)
   private val _one = getMedianOne()
   private val _two = getMedianTwo()
 
-  val number = if (_isEvenNumber) 2 else 1
+  val number = if (_isEven) 2 else 1
 
   def one = _one
   def two = _two
 
   private def getCount(): Int = {
-    if (end >= start)
+    if (end > start)
       end - start + 1
+    else if (end == start)
+      2
     else
       throw new Exception("Constrcutor param:[end] lesser than param:[start];")
   }
 
-  private def isEvenNumber(number: Int): Boolean = (number % 2) == 0
+  private def isEvenNumber(count: Int): Boolean = (count % 2) == 0
 
-  private def getMedianOne(): Int =
-    if (_isEvenNumber) _count / 2 else math.ceil(_count / 2).toInt
+  private def getMedianOne(): Int = getIndex(getOffset(_count))
 
-  private def getMedianTwo(): Int = if (_isEvenNumber) _one + 1 else _one
+  private def getMedianTwo(): Int = if (_isEven) _one + 1 else _one
+
+  private def getIndex(offset: Int): Int = offset + start - 1
+
+  private def getOffset(count: Int): Int =
+    if (_isEven) count / 2 else math.ceil(count / 2).toInt
 }
