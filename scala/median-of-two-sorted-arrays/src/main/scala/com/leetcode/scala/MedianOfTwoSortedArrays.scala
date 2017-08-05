59247bb83a241b5dadfd3b81089ada7e97f14d14
crux wild <18210841568@163.com>
2017-08-05 22:43:26 +0800

    add

--- scala/median-of-two-sorted-arrays/src/main/scala/com/leetcode/scala/MedianOfTwoSortedArrays.scala
+++ scala/median-of-two-sorted-arrays/src/main/scala/com/leetcode/scala/MedianOfTwoSortedArrays.scala
@@ -1,25 +1,46 @@
 package com.leetcode.scala
 
-import _root_.scala.collection.mutable.IndexedSeq
-
 class MedianOfTwoSortedArrays[T](val arr1: Array[T], val arr2: Array[T]) {
   private val _arr1 = arr1
   private val _arr2 = arr2
   private var _len1 = arr1.length
   private var _len2 = arr2.length
+  private val _count = _len1 + _len2
   private val _area1 = new Area(arr = arr1, start = 0, end = _len1)
   private val _area2 = new Area(arr = arr2, start = 0, end = _len2)
-  private val _count = len1 + len2
-  private val _median = new Median(start = 1, end = count)
-  private val _number = _median.number
+  private val _medianOfCount = new Median(start = 1, end = _count)
+  private val _number = _medianOfCount.number
   private val _median = getMedian()
-
   def median = _median
 
-  private def binarySplitSection(section: section): Seq[Section]] = {
-    val section1 = area.section / 2
-    var section2 = area.section - area1.section
-    Seq(section1, section2)
+  private def getSectionBefore(section: Section, end: Int): Section = {
+    var sectionBefore = section
+    var sectionSeq = Nil
+
+    if (sectionBefore.end <= end)
+      section
+    else
+      sectionSeq = binarySplitSection(section)
+      sectionBefore = sectionSeq.apply(0)
+      getSectionBefore(section = sectionBefore, end)
+  }
+
+  private def getSectionAfter(section: Section, start: Int): Section = {
+    var sectionAfter = section
+    var sectionSeq = Nil
+
+    if (sectionAfter.start >= start)
+      section
+    else
+      sectionSeq = binarySplitSection(section)
+      sectionAfter = sectionSeq.apply(1)
+      getSectionBefore(section = sectionAfter, start)
+  }
+
+  private def binarySplitSection(section: Section): Seq[Section] = {
+    val section1 = section / 2
+    var section2 = section - section
+    new Seq(section1, section2)
   }
 
   private def recursiveAreas(
@@ -28,15 +49,21 @@ class MedianOfTwoSortedArrays[T](val arr1: Array[T], val arr2: Array[T]) {
     val section2 = _area2.section
 
     val sectionSeq = binarySplitSection(section1)
+
     val section1Before = sectionSeq.apply(0)
-    val section2After = secitonSeq.apply(1)
+    val section2Before =
+      getSectionBefore(section = section2, end = section1Before.end)
+
+    val section2After = sectionSeq.apply(1)
+    val section2After =
+      getSectionAfter(section = section1, start = section2After.start)
 
     return 0.0
   }
 
   private def getMedian(): Double = {
-    val before = _median.one - 1
-    val after = _count - _median.two
+    val before = _medianOfCount.one - 1
+    val after = _count - _medianOfCount.two
     recursiveAreas(_area1, _area2, before, after)
     return 0.0
   }
