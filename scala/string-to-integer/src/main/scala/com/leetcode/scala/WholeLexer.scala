4cb2cef8d9ff38f24862f4ada0e11cfca22d5fdd
crux wild <18210841568@163.com>
2017-08-17 19:28:34 +0800

    add

--- scala/string-to-integer/src/main/scala/com/leetcode/scala/WholeLexer.scala
+++ scala/string-to-integer/src/main/scala/com/leetcode/scala/WholeLexer.scala
@@ -2,10 +2,10 @@ package com
 package leetcode
 package scala
 
-import _root_.scala.collection.mutable.{ LinkedList }
+import _root_.scala.collection.mutable.{ ListBuffer }
 
 class WholeLexer(val context: String, val lexemeBegin: Int) extends Lexer[Whole] {
-  private val _tokenList = new LinkedList[Token]()
+  private val _tokenList = new ListBuffer[Token]()
   private val _token = getToken
 
   def token = _token
@@ -19,8 +19,8 @@ class WholeLexer(val context: String, val lexemeBegin: Int) extends Lexer[Whole]
     while (true) {
       status match {
         case 0 => if (nextChar.toLower == '0') status = 1 else status =
-        case 1 => if (nextChar.toLower == 'x') status = 2 else status =
-        case 2 => if (isHexChar(nextChar.toLower)) status = 3 else status =
+        case 1 => if (nextChar.toLower == 'x') status = 2 else _tokenList += new Radix(8)
+        case 2 => if (isHexChar(nextChar.toLower)) status = 3 else _tokenList += new Radix(16)
         case 3 => if (isHexChar(nextChar.toLower)) status = 3 else status = 4
         case 4 => if ()
       }
