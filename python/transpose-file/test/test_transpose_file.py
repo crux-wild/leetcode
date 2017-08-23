ec79a2c756b687def232d1bd0cd9784cede15dcd
crux wild <18210841568@163.com>
2017-08-23 11:20:17 +0800

    add

--- python/transpose-file/test/test_transpose_file.py
+++ python/transpose-file/test/test_transpose_file.py
@@ -3,6 +3,8 @@ import unittest
 from ..src import transpose_file
 
 class TestTransposeFile(unittest.TestCase):
+    """单词矩阵转置测试用例
+    """
 
     def test_word_matrix(self):
         WordMatrix = transpose_file.WordMatrix
@@ -12,7 +14,7 @@ class TestTransposeFile(unittest.TestCase):
         transpose_matrix = word_matrix.get_transpose()
         transpose_file_content = WordMatrix.format_output_matrix(transpose_matrix)
 
-        self.assertEqual(transpose_file_content, "name alice ryan\nage 21 30\n")
+        self.assertEqual(transpose_file_content,"name alice ryan\nage 21 30\n")
 
 if __name__ == '__main__':
     unittest.main()
