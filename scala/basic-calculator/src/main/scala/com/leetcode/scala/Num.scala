d49a3d88075f1f8fc6c78766bc79e8dff2daa9af
crux wild <18210841568@163.com>
2017-09-01 14:17:20 +0800

    add

--- scala/basic-calculator/src/main/scala/com/leetcode/scala/Num.scala
+++ scala/basic-calculator/src/main/scala/com/leetcode/scala/Num.scala
@@ -6,6 +6,15 @@ package scala
  * @constructor 数字的词法单元
  * @param lexeme 对应词素
  */
-class Num(val lexmem: String) extends Token {
+class Num(val lexeme: String) extends Token {
   val tag = Tag.Num
+
+  def value = _value
+
+  private val _value = getValue
+
+  private def getValue: AnyVal = {
+    // 根据题目暂时只解析整数
+    lexeme.toInt
+  }
 }
